d708bc58cd3264ee0c559c8280ceadd4cea73e0e
arita291 <brookm291@gmail.com>
2017-06-10 16:16:23 +0900

    1st commit

---
+++ django-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script to running django application, 
+# But change the variable to corresponding environment
+
+ip="0.0.0.0:80"
+commandEx="/home/noel/project27/geoapp/geoproject/manage.py runserver"
+pythonEnv="COMPRESS=5 /home/noel/anaconda2/bin/python"
+
+$pythonEnv $commandEx $ip
+
+
