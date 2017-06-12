2a560841f5cb7a06f150f84f2f10efa8075d7b14
Handry Wahyudi <wahyudihandry@gmail.com>
2017-06-13 05:58:29 +0700

    MODIFY: add function clear, logs, and rule in django_shceduler.py

--- django_schedule.py
+++ django_schedule.py
@@ -6,11 +6,14 @@ Don't forget to change the user
 
 from crontab import CronTab
 import sys
+import subprocess
+
 
 def restart_daily():
 	myCron = CronTab(user='noel')
 
 	job = myCron.new(command='sudo /home/noel/project27/scheduler/django_service.py restart', comment='restart-django-service')
+	job.minute.on(0)
 	job.hour.on(4)
 
 	myCron.write()
@@ -20,9 +23,10 @@ def restart_daily():
 
 
 def check_daily():
-	myCron = CronTab(user='handry')
+	myCron = CronTab(user='noel')
 
 	job = myCron.new(command='sudo /home/noel/project27/scheduler/django_check.py check', comment='check-django-service')
+	job.minute.on(0)
 	job.hour.on(5)
 
 	myCron.write()	
@@ -31,14 +35,58 @@ def check_daily():
 		print jb
 
 
+def clear_rule():
+	cmd = 'crontab -r'
+	process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+	result = output
+	return result
+
+
+def show_rule():
+	cmd = 'crontab -l'
+	process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+	result = output
+	return result
+
+
+def logs(command):
+	if command == 'restart':
+		cmd = 'sudo grep restart-django-service /var/log/syslog'
+		process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+		output, error = process.communicate()
+		result = output
+		return result
+	elif command == 'check':
+		cmd = 'sudo grep check-django-service /var/log/syslog'
+		process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+		output, error = process.communicate()
+		result = output
+		return result
+	else:
+		return "You should use restart and check"
+
+	
 # main program
 arg = sys.argv[1]
+
 if arg == "restart":
+	arg = sys.argv[1]
 	restart_daily()
 elif arg == "check":
 	check_daily()
+elif arg == 'logs':
+	arg2 = sys.argv[2]
+	if arg2 == 'restart':
+		print logs('restart')
+	elif arg2 == 'check':
+		print logs('check')
+	else:
+		print "You should use restart and check"
+elif arg == 'clear':
+	print clear_rule()
+elif arg == 'rule':
+	print show_rule()
 else:
-	print 'You should use "django-schedule restart or check"'
-
-
-
+	print 'You should use "django-schedule restart, check, rule, clear and logs"'
